79a4dc4486b3ceba1b91bd04ba8c389335d252c5
Tushar Chugh <techie2sh@gmail.com>
2018-01-02 12:50:30 -0500

    add using for the allocation in the list container

--- include/forward_tlist/forward_tlist.h
+++ include/forward_tlist/forward_tlist.h
@@ -2,7 +2,6 @@
 #define FORWARD_LIST_FORWARD_TLIST_H
 
 #include "forward_tlist_node.h"
-#include <cstddef>
 #include <memory>
 
 namespace tlib{
@@ -11,11 +10,16 @@ namespace tlib{
         using allocator_type = allocator;
         using iterator = tlib::forward_tlist_iterator<T>;
         using value_type = typename allocator::value_type;
+        using pointer = typename allocator::pointer;
+        using reference = typename allocator::reference;
+        using const_reference = typename allocator::const_reference;
+        using size_type = typename allocator::size_type;
+
     public:
         forward_tlist(): head(nullptr), tail(nullptr), size_(0) {};
         ~forward_tlist() { delete head; }
         bool empty() { return head == nullptr; }
-        std::size_t size() const;
+        size_type size() const;
         /**
          * Prepends the given element to the beginning of the container
          * No iterators are invalidated
@@ -26,7 +30,7 @@ namespace tlib{
         iterator end() { return forward_tlist_iterator<T>(nullptr); }
 
     private:
-        std::size_t size_;
+        size_type size_;
         forward_tlist_node<T> *head;
         forward_tlist_node<T> *tail;
     }; //class forward_tlist
